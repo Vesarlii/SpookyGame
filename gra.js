5d330894a2dfc378a0ec2ba4f961d3a26faf16e1
Pysia <bkubinska111@gmail.com>
2024-02-03 21:32:00 +0100

    add idle animation

--- gra.js
+++ gra.js
@@ -2,118 +2,95 @@ document.addEventListener("DOMContentLoaded", function () {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
 
-  var tileset = new Image();
-  tileset.src = "tileset.png";
-
   var tileSize = 16;
-
-  var sourceX = 0;
-  var sourceY = 0;
-  var sourceWidth = tileSize;
-  var sourceHeight = tileSize;
-
-  var centerX = canvas.width / 2;
-  var centerY = canvas.height / 2;
-
-  var destinationWidth = tileSize;
-  var destinationHeight = tileSize;
-
-  var spacing = 0;
+  var tileset = new Image();
+  tileset.src = "tileset.png"; 
 
   var player = {
-    x: centerX - tileSize / 2,
+    x: 50,
     y: canvas.height - tileSize * 2 - 16,
     width: 16,
     height: 32,
     speed: 2,
-    fallSpeed: 2,
+    fallSpeed: 0,
+    jumpForce: 4,
+    isJumping: false,
+    isMoving: false,
     moveLeft: false,
-    moveRight: false
+    moveRight: false,
+    idleSprites: ["sprite_idle0.png", "sprite_idle1.png", "sprite_idle2.png", "sprite_idle3.png"],
+    currentIdleSpriteIndex: 0,
+    lastIdleAnimationTime: 0
   };
 
-  var squares = [];  // Tablica kwadratów
+  var squares = [];
 
-  tileset.onload = function () {
-    // pętla - podstawa
-    for (var i = 0; i < 5; i++) {
-      squares.push({
-        x: centerX - tileSize / 2 + i * (tileSize + spacing),
-        y: canvas.height - tileSize,
-        width: tileSize,
-        height: tileSize
-      });
-    }
+  function loadIdleSprites() {
+    player.idleSprites = player.idleSprites.map(sprite => Object.assign(new Image(), { src: "images/" + sprite }));
+  }
 
-    document.addEventListener("keydown", keyDownHandler);
-    document.addEventListener("keyup", keyUpHandler);
+  loadIdleSprites();
 
-    function keyDownHandler(e) {
-      if (e.key === "a") {
-        player.moveLeft = true;
-      }
-      if (e.key === "d") {
-        player.moveRight = true;
+  document.addEventListener("keydown", keyHandler);
+  document.addEventListener("keyup", keyHandler);
+
+  function keyHandler(e) {
+    const keys = { "a": "moveLeft", "d": "moveRight", " ": "jump" };
+    if (keys[e.key] !== undefined) {
+      player[keys[e.key]] = e.type === "keydown";
+      player.isMoving = player.moveLeft || player.moveRight;
+      if (keys[e.key] === "jump" && !player.isJumping) {
+        player.isJumping = true;
+        player.fallSpeed = -player.jumpForce;
       }
     }
+  }
 
-    function keyUpHandler(e) {
-      if (e.key === "a") {
-        player.moveLeft = false;
-      }
-      if (e.key === "d") {
-        player.moveRight = false;
-      }
+  function startIdleAnimation() {
+    if (!player.isMoving && (Date.now() - player.lastIdleAnimationTime) > 200) {
+      player.currentIdleSpriteIndex = (player.currentIdleSpriteIndex + 1) % player.idleSprites.length;
+      player.lastIdleAnimationTime = Date.now();
     }
+  }
 
-    function draw() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < canvas.width / tileSize; i++) {
+    squares.push({ x: i * tileSize, y: canvas.height - tileSize, width: tileSize, height: tileSize });
+  }
 
-      // Podstawa
-      for (var i = 0; i < squares.length; i++) {
-        ctx.drawImage(tileset, sourceX, sourceY, sourceWidth, sourceHeight, squares[i].x, squares[i].y, destinationWidth, destinationHeight);
-      }
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      // Gracz
-      ctx.fillStyle = "blue";
-      ctx.fillRect(player.x, player.y, player.width, player.height);
+    squares.forEach(square => ctx.drawImage(tileset, 0, 0, tileSize, tileSize, square.x, square.y, tileSize, tileSize));
 
-      if (player.moveLeft && player.x > 0) {
-        player.x -= player.speed;
-      }
-      if (player.moveRight && player.x + player.width < canvas.width) {
-        player.x += player.speed;
-      }
+    player.moveLeft && player.x > 0 && (player.x -= player.speed);
+    player.moveRight && player.x + player.width < canvas.width && (player.x += player.speed);
 
-
-      for (var j = 0; j < squares.length; j++) {
-        if (checkCollision(player, squares[j])) {
-
-          player.y = squares[j].y - player.height;
-
-
-          player.fallSpeed = 0;
+    squares.forEach(square => {
+      if (checkCollision(player, square)) {
+        player.y = square.y - player.height;
+        player.fallSpeed = 0;
+        player.isJumping = false;
+        if (!player.isMoving) {
+          startIdleAnimation();
         }
       }
+    });
 
-      // spadanie
-      if (player.y < canvas.height - player.height) {
-        player.y += player.fallSpeed;
-        player.fallSpeed += 0.1; 
-      }
+    player.y + player.height >= canvas.height && (player.y = canvas.height - player.height, player.fallSpeed = 0, player.isJumping = false, !player.isMoving && startIdleAnimation());
 
-      requestAnimationFrame(draw);
-    }
+    player.y += player.fallSpeed;
+    player.fallSpeed += 0.1;
 
+    ctx.drawImage(player.idleSprites[player.currentIdleSpriteIndex], player.x, player.y, player.width, player.height);
+
+    requestAnimationFrame(draw);
+  }
+
+  tileset.onload = function () {
     draw();
   };
 });
 
-// Kolizja
 function checkCollision(rect1, rect2) {
-  return (
-    rect1.x < rect2.x + rect2.width &&
-    rect1.x + rect1.width > rect2.x &&
-    rect1.y < rect2.y + rect2.height &&
-    rect1.y + rect1.height > rect2.y
-  );
+  return rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y;
 }
